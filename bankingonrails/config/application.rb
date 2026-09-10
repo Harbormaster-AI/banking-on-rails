ec3c63eb3d3ba1ecfb335e479e631226b069aead
Ethan Williams <ethan.williams@mobilefirst.app>
2026-09-10 12:16:53 -0400

    Initial commit

--- bankingonrails/config/application.rb
+++ bankingonrails/config/application.rb
@@ -4,7 +4,7 @@ require "rails/all"
 
 Bundler.require(*Rails.groups)
 
-module ${ Utils.capitalizeFirstLetter( bankingonrails ) }
+module Bankingonrails }
   class Application < Rails::Application
     config.load_defaults 8.1
 
