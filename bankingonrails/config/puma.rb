084ded6ed47fbf7c7802af9c8ac1b209dc0b4135
Ethan Williams <ethan.williams@mobilefirst.app>
2026-09-11 18:30:00 -0400

    Initial commit

--- bankingonrails/config/puma.rb
+++ bankingonrails/config/puma.rb
@@ -4,7 +4,7 @@ min_threads_count = ENV.fetch("RAILS_MIN_THREADS", max_threads_count)
 
 threads min_threads_count, max_threads_count
 
-port ENV.fetch("PORT", 5432)
+port ENV.fetch("PORT", 3000)
 
 environment ENV.fetch("RAILS_ENV", "development")
 
